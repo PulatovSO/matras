58cf265ec16804da1f50e9e56fabe27f8533815c
So coding <pulatovsirotillo@gmail.com>
2022-04-22 11:29:12 +0500

    finished footer

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 let menuBtn = document.querySelectorAll('.menu-btn');
 let menuBar = document.querySelector('.menu');
 let body = document.querySelector('.body');
+let success = document.querySelector('.success');
+let messageForm = document.querySelector('.message__right');
 
 let menuState = true;
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    success.style.transform = 'translate(-100%, -50%)';
+})
 
 menuBtn.forEach(item => {
     item.addEventListener('click', () => {
         if (menuState == false) {
+            console.log('worked')
             menuBar.style.transform = 'translateY(-200%)';
-            // body.style.overflow = 'scroll';
         } else {
             menuBar.style.transform = 'translateY(0)';
-            // body.style.overflow = 'hidden';
         }
         menuState = !menuState;
     })
 })
+
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false
+    });
+  });
